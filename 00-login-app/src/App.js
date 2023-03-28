bf125e1605f3e7212fa1b04f11b1e7f28ab13583
keekerr <keeley.s.sprouse@gmail.com>
2023-03-28 19:34:58 -0400

    Added file structure to support animations

---
+++ 00-login-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+function App() {
+  return <AppContainer>ello World!</AppContainer>;
+  
+}
+
+export default App;
