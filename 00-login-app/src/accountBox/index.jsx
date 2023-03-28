a647a97734d720db16fb82ea1a511fd1655c06b7
keekerr <keeley.s.sprouse@gmail.com>
2023-03-28 19:56:02 -0400

    Added code to animation styling

--- 00-login-app/src/accountBox/index.jsx
+++ 00-login-app/src/accountBox/index.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import styled from "styled-components";
 
-const BoxContainer = styled.div``
+const BoxContainer = styled.div`
+    width: 280px;
+    min-height: 550px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 19px;
+    background-color: #fff;
+    box-shadow: 0 0 2px rgba(15, 15, 15, 0.28);
+    position: relative;
+    overflow: hidden;
+`;
+
+const TopContainer = styled.div`
+    width: 100%;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    padding: 0 1.8em;
+    padding-bottom: 5em;
+`;
+
+const BackDrop = styled.div`
+    width: 160%;
+    height: 550px;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    border-radius: 50%;
+`;
 
 export function AccountBox(props){
-
+    return <BoxContainer>
+        <TopContainer>
+            <BackDrop/>
+        </TopContainer>
+    </BoxContainer>
 }
