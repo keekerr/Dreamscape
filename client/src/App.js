a905d63735dbc5acc5ffe9e7c153c7fcc879c2e2
n-lofaso <nicklofaso96@gmail.com>
2023-03-30 19:51:17 -0400

    got some of the routes working

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-import Diary from  './pages/Diary'
-import DiaryEntry from './components/DiaryEntry'
+import Diary from './pages/Diary';
+import VisionBoard from './pages/VisionBoard'
+import DiaryEntry from './components/DiaryEntry';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    <NavBar />
-      <Diary />
-      <DiaryEntry />
-    </div>
+    <Router>
+      <div className='App'>
+        <NavBar />
+        <div className='page-container'>
+          <Routes>
+            <Route path='/diary' element={<Diary />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
-
 
 export default App;
