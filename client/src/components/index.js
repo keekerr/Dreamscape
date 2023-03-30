cd3f63ed1a858d7804f5d6d88674abc33adec2d0
keekerr <keeley.s.sprouse@gmail.com>
2023-03-30 19:16:19 -0400

    Updated File Structure

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from '../../../00-login-app/src/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
