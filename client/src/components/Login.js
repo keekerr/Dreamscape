1215e5396e5c54befd2ac70bf265d19f3bdda293
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-03-30 19:28:01 -0400

    Added logic to loginForm.jsx

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,42 +1,6 @@
 import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
+
 
 const Login = () => {
-    const [userData, setUserData] = useState({ email: '', password: '' });
-    const [validated] = useState(false);
 
-    const [loginUser] = useMutation(LOGIN_USER);
-
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setUserData({ ...userData, [name]: value });
-    };
-
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-
-        try {
-            const { data } = await loginUser({
-                variables: { ...userData }
-            });
-
-            console.log(data);
-            Auth.login(data.loginUser.token);
-        } catch (err) {
-            console.error(err);
-        }
-
-        setUserData({
-            email: '',
-            password: '',
-        });
-    }
 }
