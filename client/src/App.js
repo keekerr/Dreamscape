cfac7c782e2d55873c7ca3ab361a481169f22537
ALYSSA_MAPLES <raynemaples@gmail.com>
2023-04-02 12:44:04 -0400

    Added padding and centering to pages

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import DiaryEntry from './components/DiaryEntry';
 import { BrowserRouter as Router, Routes, Route, Navigate  } from 'react-router-dom';
 import { AccountBox } from "./components/accountBox/accountBox";
 import "./App.css";
+import styled from "styled-components";
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -30,6 +31,16 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 50px;
+`;
 
 function App() {
   const [query, setQuery] = useState('');
@@ -64,14 +75,14 @@ function App() {
     <Router>
       <div className='App'>
         <NavBar />
-        <div className='page-container'>
+        <AppContainer>
           <Routes>
             <Route path='/*' element={<Navigate replace to="/visionboard" />} />
             <Route path='/diary' element={<Diary />} />
             <Route path='/visionboard' element={<VisionBoard />} />
             <Route path='/login-signup' element={<AccountBox />} />
           </Routes>
-        </div>
+        </AppContainer>
       </div>
     </Router>
     </ApolloProvider>
